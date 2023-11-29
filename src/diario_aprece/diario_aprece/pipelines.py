3cc9768b1c78e5bc6324f9005bced0dc25b77300
joseandre25 <andrerabelo2503@gmail.com>
2023-11-29 11:35:04 -0300

    :fire: front não consegue ler JSON

--- src/diario_aprece/diario_aprece/pipelines.py
+++ src/diario_aprece/diario_aprece/pipelines.py
@@ -1,31 +1,31 @@
 import os
 import json
+from unidecode import unidecode
+from collections import defaultdict
 
 class DiarioPipeline:
     def open_spider(self, spider):
-        self.results = {}  # Inicialize um dicionário vazio para os resultados
+        self.results = defaultdict(list)  # Use defaultdict para evitar verificações desnecessárias de existência de chave
 
     def process_item(self, item, spider):
         # Extraia os detalhes do item
-        pdf_filename = item['pdf_filename']
         municipio = item['municipio']
         data_pdf = item['data_pdf']
         num_avisos_licitacao = item['num_avisos_licitacao']
 
         # Crie um dicionário para armazenar os dados
         data_dict = {
-            "pdf_filename": pdf_filename,
             "municipio": municipio,
             "data_pdf": data_pdf,
             "num_avisos_licitacao": num_avisos_licitacao
         }
 
-        # Se o município já existe no dicionário, adicione os dados ao município existente
-        if municipio in self.results:
-            self.results[municipio].append(data_dict)
-        else:
-            # Caso contrário, crie uma lista para o município e adicione os dados
-            self.results[municipio] = [data_dict]
+        # Remova acentos e caracteres especiais do nome do município para uso no nome do arquivo JSON
+        clean_municipio = unidecode(municipio.lower()).replace(' ', '-')
+
+        # Adicione os dados ao dicionário usando o mês como chave
+        month_key = data_pdf.split()[2]  # Obtém o mês do campo 'data_pdf'
+        self.results[clean_municipio].append(data_dict)
 
         return item
 
@@ -35,8 +35,8 @@ class DiarioPipeline:
         os.makedirs(output_dir, exist_ok=True)
 
         # Para cada município, carregue os resultados antigos (se existirem)
-        for municipio, data_list in self.results.items():
-            output_file = os.path.join(output_dir, f'{municipio.lower()}_resultados.json')
+        for clean_municipio, data_list in self.results.items():
+            output_file = os.path.join(output_dir, f'{clean_municipio}.json')
 
             # Tente carregar os resultados antigos se o arquivo existir
             old_data = []
@@ -44,9 +44,36 @@ class DiarioPipeline:
                 with open(output_file, 'r', encoding='utf-8') as json_file:
                     old_data = json.load(json_file)
 
+            # Remova o campo "pdf_filename" de todos os dicionários na lista
+            data_list_no_filename = [
+                {key: value for key, value in data.items() if key != 'pdf_filename'}
+                for data in data_list
+            ]
+
             # Adicione os novos resultados aos resultados antigos
-            all_data = old_data + data_list
+            all_data = old_data + data_list_no_filename
 
             # Salve os resultados combinados em um único arquivo JSON
             with open(output_file, 'w', encoding='utf-8') as json_file:
                 json.dump(all_data, json_file, indent=2, default=str, ensure_ascii=False)
+
+            # Calcular a soma dos num_avisos_licitacao para cada mês
+            monthly_sums = defaultdict(int)
+            for data in all_data:
+                month_key = data['data_pdf'].split()[2]
+                monthly_sums[month_key] += data['num_avisos_licitacao']
+
+            # Adicionar a soma mensal ao dicionário
+            monthly_sums_data = [
+                {
+                    'municipio': clean_municipio,
+                    'month': month,
+                    'sum_num_avisos_licitacao': total
+                }
+                for month, total in monthly_sums.items()
+            ]
+
+            # Salvar o arquivo com a soma mensal
+            monthly_sums_output_file = os.path.join(output_dir, f'{clean_municipio}_monthly_sums.json')
+            with open(monthly_sums_output_file, 'w', encoding='utf-8') as monthly_sums_json_file:
+                json.dump(monthly_sums_data, monthly_sums_json_file, indent=2, default=str, ensure_ascii=False)
